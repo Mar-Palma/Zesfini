f184b9450741b6d02ca6f7d70e8c49aadf0d0c39
Mar-Palma <tamara.palma.hernandez@gmail.com>
2013-10-25 03:17:58 -0300

    Sin Jquery
    No funciona con jQuery - en index2 funciona, pero solo ejecuta la
    primera acción (Alert que funciona jQuery). De ninguna forma ni manera.
    Mañana revisar. Responsive completo.

---
+++ js/effect.js
@@ -0,0 +1,8 @@
+    <script type="text/javascript">
+    $('main_content').click(function () {
+        $(this).fadeOut('fast');
+    });
+    $('main_content').hover(function () {
+    $(this).addClass('orange');
+    });
+});
